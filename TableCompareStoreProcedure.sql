b239f811d7d2f45d5953466eb7803ca1fd398d99
chipslam-maker <chipslam@gmail.com>
2026-01-15 13:20:02 +0000

    Update TableCompareStoreProcedure.sql

--- TableCompareStoreProcedure.sql
+++ TableCompareStoreProcedure.sql
@@ -1,8 +1,8 @@
-CREATE PROCEDURE dbo.sp_CompareTables_CompositeKey
+ALTER PROCEDURE dbo.sp_CompareTables_CompositeKey
     @SourceDB NVARCHAR(128),
     @TargetDB NVARCHAR(128),
     @TableName NVARCHAR(128),
-    @PKColumns NVARCHAR(MAX),  -- 支持多個 PK，用逗號隔開，例如 'ItemID,SRC'
+    @PKColumns NVARCHAR(MAX),  -- 例如 'ItemID,SRC'
     @IgnoreColumns NVARCHAR(MAX)
 AS
 BEGIN
@@ -12,31 +12,40 @@ BEGIN
     DECLARE @PKList TABLE (PKName NVARCHAR(128));
     DECLARE @DynamicSQL NVARCHAR(MAX) = '';
     
-    -- 1. 解析 PK 列表與 Ignore 列表
+    -- 1. 解析 PK 列表 (確保去掉多餘空格)
     DECLARE @PKXML XML = CAST('<x>' + REPLACE(@PKColumns, ',', '</x><x>') + '</x>' AS XML);
-    DECLARE @IgnoreXML XML = CAST('<x>' + REPLACE(@IgnoreColumns, ',', '</x><x>') + '</x>' AS XML);
+    INSERT INTO @PKList 
+    SELECT LTRIM(RTRIM(n.value('.', 'NVARCHAR(128)'))) 
+    FROM @PKXML.nodes('x') AS T(n);
 
-    INSERT INTO @PKList SELECT LTRIM(RTRIM(n.value('.', 'NVARCHAR(128)'))) FROM @PKXML.nodes('x') AS T(n);
+    -- 2. 解析 Ignore 列表
+    DECLARE @IgnoreXML XML = CAST('<x>' + REPLACE(ISNULL(@IgnoreColumns,''), ',', '</x><x>') + '</x>' AS XML);
+    DECLARE @IgnoreTable TABLE (ColName NVARCHAR(128));
+    INSERT INTO @IgnoreTable 
+    SELECT LTRIM(RTRIM(n.value('.', 'NVARCHAR(128)'))) 
+    FROM @IgnoreXML.nodes('x') AS T(n);
 
-    -- 2. 抓取一般欄位 (排除所有 PK 和 Ignore 欄位)
+    -- 3. 抓取一般欄位
     INSERT INTO @ColumnList
     SELECT name FROM sys.columns WHERE object_id = OBJECT_ID(@TableName)
     AND name NOT IN (SELECT PKName FROM @PKList)
-    AND name NOT IN (SELECT LTRIM(RTRIM(n.value('.', 'NVARCHAR(128)'))) FROM @IgnoreXML.nodes('x') AS T(n));
+    AND name NOT IN (SELECT ColName FROM @IgnoreTable);
 
-    -- 3. 構建動態 JOIN 條件與 PK 資訊顯示
+    -- 4. 構建動態 JOIN 條件與 PK 資訊顯示
     DECLARE @JoinClause NVARCHAR(MAX) = '';
     DECLARE @PKInfoClause NVARCHAR(MAX) = '';
 
-    SELECT @JoinClause = @JoinClause + 'a.' + QUOTENAME(PKName) + ' = b.' + QUOTENAME(PKName) + ' AND ',
-           @PKInfoClause = @PKInfoClause + '''' + PKName + ': '' + CAST(COALESCE(a.' + QUOTENAME(PKName) + ', b.' + QUOTENAME(PKName) + ') AS NVARCHAR(MAX)) + '' | '' + '
+    -- 這裡使用了更安全的方式來串接字串，避免結尾出現多餘符號
+    SELECT @JoinClause = @JoinClause + 'a.' + QUOTENAME(PKName) + ' = b.' + QUOTENAME(PKName) + ' AND '
     FROM @PKList;
+    SET @JoinClause = LEFT(@JoinClause, LEN(@JoinClause) - 4); -- 移除最後的 ' AND '
 
-    SET @JoinClause = LEFT(@JoinClause, LEN(@JoinClause) - 4); -- 移除最後的 AND
-    SET @PKInfoClause = LEFT(@PKInfoClause, LEN(@PKInfoClause) - 7); -- 移除最後的分隔符
+    -- 構建 PK 資訊，例如 'ID: 1 | SRC: A'
+    SELECT @PKInfoClause = @PKInfoClause + '''' + PKName + ': '' + CAST(ISNULL(a.' + QUOTENAME(PKName) + ', b.' + QUOTENAME(PKName) + ') AS NVARCHAR(MAX)) + '' | '' + '
+    FROM @PKList;
+    SET @PKInfoClause = LEFT(@PKInfoClause, LEN(@PKInfoClause) - 7); -- 移除最後的 ' | ' +
 
-    -- 4. 處理「只有 A 有」或「只有 B 有」
-    -- 利用第一個 PK 欄位來判斷是否存在
+    -- 5. 處理「只有 A 有」或「只有 B 有」
     DECLARE @FirstPK NVARCHAR(128);
     SELECT TOP 1 @FirstPK = PKName FROM @PKList;
 
@@ -49,9 +58,9 @@ BEGIN
         ''[Row Presence]'', NULL, NULL
     FROM ' + QUOTENAME(@SourceDB) + '.dbo.' + QUOTENAME(@TableName) + ' a
     FULL OUTER JOIN ' + QUOTENAME(@TargetDB) + '.dbo.' + QUOTENAME(@TableName) + ' b ON ' + @JoinClause + '
-    WHERE a.' + QUOTENAME(@FirstPK) + ' IS NULL OR b.' + QUOTENAME(@FirstPK) + ' IS NULL;';
+    WHERE a.' + QUOTENAME(@FirstPK) + ' IS NULL OR b.' + QUOTENAME(@FirstPK) + ' IS NULL;'
 
-    -- 5. 逐一欄位比對
+    -- 6. 逐一欄位比對
     DECLARE @ColName NVARCHAR(128);
     DECLARE col_cursor CURSOR FOR SELECT ColName FROM @ColumnList;
     OPEN col_cursor;
@@ -70,7 +79,11 @@ BEGIN
             ISNULL(CAST(b.' + QUOTENAME(@ColName) + ' AS NVARCHAR(MAX)), ''[[NULL]]'')
         FROM ' + QUOTENAME(@SourceDB) + '.dbo.' + QUOTENAME(@TableName) + ' a
         INNER JOIN ' + QUOTENAME(@TargetDB) + '.dbo.' + QUOTENAME(@TableName) + ' b ON ' + @JoinClause + '
-        WHERE EXISTS (SELECT a.' + QUOTENAME(@ColName) + ' COLLATE DATABASE_DEFAULT EXCEPT SELECT b.' + QUOTENAME(@ColName) + ' COLLATE DATABASE_DEFAULT);';
+        WHERE EXISTS (
+            SELECT a.' + QUOTENAME(@ColName) + ' COLLATE DATABASE_DEFAULT 
+            EXCEPT 
+            SELECT b.' + QUOTENAME(@ColName) + ' COLLATE DATABASE_DEFAULT
+        );'
 
         FETCH NEXT FROM col_cursor INTO @ColName;
     END
@@ -78,5 +91,7 @@ BEGIN
     CLOSE col_cursor;
     DEALLOCATE col_cursor;
 
+    -- 執行前先 PRINT 出來，你可以 Copy PRINT 出來的結果去手動 Run，看哪裡報錯
+    -- PRINT @DynamicSQL; 
     EXEC sp_executesql @DynamicSQL;
 END
